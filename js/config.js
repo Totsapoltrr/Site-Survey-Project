407f07839b5a58dfe04ffe5aac57d20f23bc1ba0
Totsapol Choksakulwong <TotsapolC@goldenduckgroup.com>
2026-08-17 17:10:18 +0700

    Configure Firebase Realtime Cloud Database for multi-device sync

--- js/config.js
+++ js/config.js
@@ -1,14 +1,14 @@
 /**
  * config.js - Global App Configuration
- * คุณสามารถนำ Firebase Config จาก Firebase Console มาวางที่นี่
- * เพื่อให้ทุกคนที่เปิดเว็บผ่าน GitHub Pages เชื่อมต่อฐานข้อมูลเดียวกันอัตโนมัติ
+ * Firebase Realtime Cloud Database Config
  */
 window.FIREBASE_CONFIG = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyBZekC2zuSuJyYHFksIpp5uxLyJobHNYec",
+  authDomain: "sitesurveyproject-a0777.firebaseapp.com",
+  databaseURL: "https://sitesurveyproject-a0777-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "sitesurveyproject-a0777",
+  storageBucket: "sitesurveyproject-a0777.firebasestorage.app",
+  messagingSenderId: "960490348597",
+  appId: "1:960490348597:web:ce9ed7bfc81d0b8937d17c",
+  measurementId: "G-PSY5C4BVQC"
 };
